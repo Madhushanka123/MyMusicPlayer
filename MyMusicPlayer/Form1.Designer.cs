332168922382834d1d984566cbac897cf7f9c323
DESKTOP-MNJ15UQ\Madhushanka <shainemy@gmail.com>
2019-05-28 14:47:34 +0530

    Initial commit

---
+++ MyMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MyMusicPlayer
+{
+	partial class MyMusicPlayer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyMusicPlayer));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.listBox = new System.Windows.Forms.ListBox();
+			this.selectSongBtn = new System.Windows.Forms.Button();
+			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.DimGray;
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(659, 37);
+			this.panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(104, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Music Player";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::MyMusicPlayer.Properties.Resources.icons8_Shutdown_48px_1;
+			this.pictureBox1.Location = new System.Drawing.Point(612, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(35, 34);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// listBox
+			// 
+			this.listBox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+			this.listBox.ForeColor = System.Drawing.Color.White;
+			this.listBox.FormattingEnabled = true;
+			this.listBox.Location = new System.Drawing.Point(510, 40);
+			this.listBox.Name = "listBox";
+			this.listBox.Size = new System.Drawing.Size(149, 264);
+			this.listBox.TabIndex = 1;
+			this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+			// 
+			// selectSongBtn
+			// 
+			this.selectSongBtn.BackColor = System.Drawing.Color.Gray;
+			this.selectSongBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.selectSongBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.selectSongBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.selectSongBtn.Location = new System.Drawing.Point(510, 310);
+			this.selectSongBtn.Name = "selectSongBtn";
+			this.selectSongBtn.Size = new System.Drawing.Size(149, 42);
+			this.selectSongBtn.TabIndex = 2;
+			this.selectSongBtn.Text = "Select Songs";
+			this.selectSongBtn.UseVisualStyleBackColor = false;
+			this.selectSongBtn.Click += new System.EventHandler(this.selectSongBtn_Click);
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			this.axWindowsMediaPlayer1.Enabled = true;
+			this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 40);
+			this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+			this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(504, 312);
+			this.axWindowsMediaPlayer1.TabIndex = 3;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Black;
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel2.Location = new System.Drawing.Point(0, 358);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(659, 28);
+			this.panel2.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.Silver;
+			this.label2.Location = new System.Drawing.Point(234, 6);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(145, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Developed By Madhushanka";
+			// 
+			// MyMusicPlayer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Silver;
+			this.ClientSize = new System.Drawing.Size(659, 386);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.axWindowsMediaPlayer1);
+			this.Controls.Add(this.selectSongBtn);
+			this.Controls.Add(this.listBox);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MyMusicPlayer";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Music Player";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox;
+		private System.Windows.Forms.Button selectSongBtn;
+		private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ MyMusicPlayer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMusicPlayer
+{
+	public partial class MyMusicPlayer : Form
+	{
+		public MyMusicPlayer()
+		{
+			InitializeComponent();
+		}
+		String[] paths, files;
+
+		private void selectSongBtn_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Multiselect = true;
+			if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				files = ofd.SafeFileNames;
+				paths = ofd.FileNames;
+				for(int i = 0; i < files.Length; i++)
+				{
+					listBox.Items.Add(files[i]);
+				}
+			}
+		}
+
+		private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			axWindowsMediaPlayer1.URL = paths[listBox.SelectedIndex];
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
